91fcc39852ed1566bb0b31b881274f94a89f696d
Daniel's Nguyen <macbookpro@BeON.local>
2023-05-16 10:46:39 +0700

    MovingObject

---
+++ Assets/Movement.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    public Transform target;
+    public float speed;
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+     void Update()
+    {
+        Vector3 a = transform.position;
+        Vector3 b = target.position;
+        transform.position = Vector3.MoveTowards(a, b, speed);
+    }
+}
