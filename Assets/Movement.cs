bb15d75268af2ac6d10639d31dec50258f29ded9
Daniel's Nguyen <macbookpro@BeON.local>
2023-05-16 17:40:43 +0700

    MovingByKey

--- Assets/Movement.cs
+++ Assets/Movement.cs
@@ -6,7 +6,8 @@ public class Movement : MonoBehaviour
 {
     public Transform target;
     public float speed;
-
+    public float horizontalInput;
+    public float verticalInput;
 
 
     // Start is called before the first frame update
@@ -18,8 +19,28 @@ public class Movement : MonoBehaviour
     // Update is called once per frame
      void Update()
     {
+
+        // moving from A to B
+        //MovingFromAToB();
+
+        // use arrow key to move
+        MovingByKey();
+    }
+
+    void MovingFromAToB()
+    {
         Vector3 a = transform.position;
         Vector3 b = target.position;
         transform.position = Vector3.MoveTowards(a, b, speed);
     }
+
+    void MovingByKey()
+    {
+        horizontalInput = Input.GetAxis("Horizontal");
+        verticalInput = Input.GetAxis("Vertical");
+        transform.Translate(Vector3.forward * speed * verticalInput);
+        transform.Translate(Vector3.right * speed * horizontalInput);
+    }
+
+
 }
