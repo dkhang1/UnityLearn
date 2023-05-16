3cb4117ae306365466236bfef67fcc933bd1eb7d
Daniel's Nguyen <macbookpro@BeON.local>
2023-05-16 17:56:33 +0700

    Jump

--- Assets/Movement.cs
+++ Assets/Movement.cs
@@ -6,18 +6,23 @@ public class Movement : MonoBehaviour
 {
     public Transform target;
     public float speed;
-    public float horizontalInput;
-    public float verticalInput;
+    public float jumpForce;
+    private float horizontalInput;
+    private float verticalInput;
+    private bool jumpInput;
+
+    private Rigidbody rb;
+
 
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
-     void Update()
+    void Update()
     {
 
         // moving from A to B
@@ -31,16 +36,27 @@ public class Movement : MonoBehaviour
     {
         Vector3 a = transform.position;
         Vector3 b = target.position;
-        transform.position = Vector3.MoveTowards(a, b, speed);
+        transform.position = Vector3.MoveTowards(a, b, speed*Time.deltaTime);
     }
 
     void MovingByKey()
     {
+        // Moving
         horizontalInput = Input.GetAxis("Horizontal");
         verticalInput = Input.GetAxis("Vertical");
-        transform.Translate(Vector3.forward * speed * verticalInput);
-        transform.Translate(Vector3.right * speed * horizontalInput);
+        transform.Translate(Vector3.forward * speed * Time.deltaTime * verticalInput);
+        transform.Translate(Vector3.right * speed * Time.deltaTime * horizontalInput);
+
+        // Jump
+        jumpInput = Input.GetKeyDown(KeyCode.Space);
+        if (jumpInput)
+        {
+            rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+        }
+
     }
+
+
 
 
 }
